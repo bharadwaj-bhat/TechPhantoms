559805afa8c4ba5dd0f3e93589bff159897e3a4a
pallav1998 <jain.pallav1998@gmail.com>
2021-10-09 10:54:13 +0530

    Footer Done

---
+++ frontEnd/src/Component/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillInstagram,
+  AiFillFacebook,
+} from "react-icons/ai";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footerbody">
+      <div className="footerlink">
+        <div>
+          {/* <Link to="/">
+            <img
+              height="40px"
+              src="https://static.frontendmasters.com/assets/fm/js/frontendmasters.e1e10b58c1.svg"
+              alt="LOGO"
+            />
+          </Link> */}
+        </div>
+      </div>
+
+      <div className="social">
+        <AiFillTwitterCircle /> &nbsp;
+        <AiFillLinkedin /> &nbsp;
+        <AiFillInstagram />
+        &nbsp;
+        <AiFillFacebook />
+      </div>
+
+      <p> © 2021 XYZ Masters · Terms of Service · Privacy Policy </p>
+    </div>
+    //   </div>
+  );
+}
+
+export { Footer };
